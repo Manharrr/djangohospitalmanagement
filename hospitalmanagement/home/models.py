3d2dc8e4b335a85ce2f0ddd30e815dc05406580b
ManharCK <manhargurukkal2@gmail.com>
2026-03-10 17:11:20 +0530

    updated

--- hospitalmanagement/home/models.py
+++ hospitalmanagement/home/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Departments(models.Model):
+    dep_name=models.CharField(max_length=50)
+    dep_description=models.TextField()
+
+    def __str__(self):
+         return self.dep_name
+
+   
+
+class Doctors(models.Model):
+    doc_name=models.CharField(max_length=100)
+    doc_spec=models.CharField(max_length=200)
+    dep_name=models.ForeignKey(Departments,on_delete=models.CASCADE)
+    def __str__(self):
+        return'DR'+ self.doc_name +'-('+ self.doc_spec+')'
+    # # image=models.ImageField(upload_to='doctors') #pillow library for image
+
+class Booking(models.Model):
+    pname= models.CharField(max_length=50)
+    pphone=models.IntegerField()
+    eemail=models.EmailField()
+    docname=models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    bookingdate=models.DateField()
+    bookedon=models.DateField(auto_now=True)
+
+
+
--- hospitalmanagement/home/views.py
+++ hospitalmanagement/home/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 # Create your views here.
-
-
+from .models import Departments,Doctors
 
 
 def index(request):
     return render(request,'index.html')
+
 def about(request):
     return render(request,'about.html')
+
 def booking(request):
     return render(request,'booking.html')
+
 def contact(request):
     return render(request,'contact.html')
+
 def department(request):
-    return render(request,'department.html')
+    dept={
+        'dept':Departments.objects.all()
+    }
+
+    return render(request,'department.html',dept)
+
 def doctors(request):
-    return render(request,'doctors.html')
+    doc = {
+        'doctors':Doctors.objects.all()
+    }
+    return render(request,'doctors.html',doc)
 
 
 
